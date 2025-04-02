9eb8a2c68fb98202187a55f4fe496a96943024e0
bleartsejdiu <69080739+bleartsejdiu@users.noreply.github.com>
2025-04-02 14:03:47 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
-(function ($) {
-    "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
     new WOW().init();
 
-
-    // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 45) {
             $('.navbar').addClass('sticky-top shadow-sm');
@@ -26,7 +9,6 @@
     });
     
     
-    // Dropdown on mouse hover
     const $dropdown = $(".dropdown");
     const $dropdownToggle = $(".dropdown-toggle");
     const $dropdownMenu = $(".dropdown-menu");
@@ -54,7 +36,6 @@
     });
     
     
-    // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
@@ -68,7 +49,6 @@
     });
 
 
-    // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
@@ -89,6 +69,5 @@
             }
         }
     });
-    
-})(jQuery);
+
 
